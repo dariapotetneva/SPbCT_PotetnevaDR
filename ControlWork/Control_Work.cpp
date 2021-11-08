0d9af743c085c29d8843837e67d70624d043bc85
dariapotetneva <93891534+dariapotetneva@users.noreply.github.com>
2021-11-08 04:22:34 +0300

    Add files via upload

---
+++ ControlWork/Control_Work.cpp
@@ -0,0 +1,41 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    setlocale(LC_ALL, ".1251");
+    FILE* f = fopen("1.txt", "r");
+    char* wave;
+    char ch[100];
+    int c = -1,
+        max = strtoll(argv[2], &wave, 20);
+    cout << "Authentic file:\n";
+    while (true)
+    {
+        c++;
+        ch[c] = fgetc(f);
+        if (ch[c] == EOF)
+            break;
+        cout << ch[c] << " ";
+    }
+    fclose(f);
+    f = fopen("2.txt", "w");
+    cout << "\nUpdated file:\n";
+    for (int i = 0; i < c; i++) {
+        if (i == max) {
+            fprintf(f, "Y");
+            cout << "Y";
+        }
+        fprintf(f, "%c", ch[i]);
+        cout << ch[i] << " ";
+    }
+    if (c <= max) {
+        fprintf(f, "Y");
+        cout << "Y";
+    }
+    fclose(f);
+    return 0;
+}
